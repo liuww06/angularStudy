8c54054ffafdbc7c5c7c12a2a682e447197c64e8
wenwei.liu <wenwei.liu@danikor.com>
2019-04-19 17:14:24 +0800

    1、测试

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROS } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes = HEROS;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+}
