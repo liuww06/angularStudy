ce0376d84189a63c4a7fe23bd7ca9668f704893e
unknown <wenwei.liu@danikor.com>
2019-04-22 15:17:21 +0800

    路由

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HEROS } from './mock-heroes';
 import { Hero } from './hero';
-import {Observable,of } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 
 
@@ -10,8 +11,13 @@ import {Observable,of } from 'rxjs';
 })
 export class HeroService {
 
-  constructor() { }
-  getHeros():Observable<Hero[]>{
+  constructor(private messageService: MessageService) { }
+  getHeros(): Observable<Hero[]> {
+    this.messageService.add('HeroService:fetched heroes');
     return of(HEROS);
+  }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService:fetched hero id=${id}`);
+    return of(HEROS.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,18 +9,14 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
-  heroes : Hero[];
-  constructor(private heroService:HeroService) { }
+  heroes: Hero[];
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
     this.getHeros();
   }
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
-  getHeros():void{
-     this.heroService.getHeros().subscribe(heros=>this.heroes = heros);
+  getHeros(): void {
+    this.heroService.getHeros().subscribe(heros => this.heroes = heros);
   }
 
 }
